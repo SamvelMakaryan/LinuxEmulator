4b12aa2841c1fb2a823e655e6af486d1fb11e435
SamvelMakaryan <samvelmakaryan721@gmail.com>
2023-07-19 17:36:11 +0400

    adding opening

--- Emulator.cpp
+++ Emulator.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include <string>
+
 #include "System.hpp"
 #include "File.hpp"
 #include "TxtFile.hpp"
 #include "Directory.hpp"
+#include "Opening.hpp"
 
 int main() {
     System sys;
     std::string input;
+	opening();
     while (true) {
         std::cout << "\033[1;92m" << System::getCurrentUser()->getName() << "@hp:" << "\033[1;94m" << sys.getCurrentPath() << "\033[95m$ "; 
         std::getline(std::cin, input);
